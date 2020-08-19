e3bff1f59f7bc737df7f58bc43c9f06157be3919
Vito Gamberini <vito@gamberini.email>
2020-08-19 04:26:45 -0400

    ssh plz

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -1,10 +1,7 @@
-choco install python
-refreshenv
-choco install pip
-choco install cmake ninja visualstudio2019buildtools
+choco install python pip cmake ninja visualstudio2019buildtools
 refreshenv
 
-git clone git@github.com:microsoft/vcpkg.git
+git clone https://github.com/microsoft/vcpkg.git
 cd vcpkg
 ./bootstrap-vcpkg.bat
 ./vcpkg install botan

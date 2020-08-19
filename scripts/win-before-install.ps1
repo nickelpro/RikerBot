d766b8dcb187963601238d81152126b5ad313a42
Vito Gamberini <vito@gamberini.email>
2020-08-19 08:37:27 -0400

    lookaround

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -4,8 +4,8 @@ if (Test-Path($ChocolateyProfile)) {
 }
 choco install python swig cmake ninja
 choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
-Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio"
-Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\Shared"
+Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019"
+Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools"
 refreshenv
 pip install wheel
 pip install conan

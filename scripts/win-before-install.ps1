cc6f1622bbc16320c49dcaad80d992b9df7e9827
Vito Gamberini <vito@gamberini.email>
2020-08-19 08:53:32 -0400

    keep trying

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -5,7 +5,7 @@ if (Test-Path($ChocolateyProfile)) {
 choco install python swig cmake ninja
 choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild"
-Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7"
+Get-ChildItem -Path "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools"
 refreshenv
 pip install wheel
 pip install conan
--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,4 +1,6 @@
 $Env:VS160COMNTOOLS = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7"
+$Env:CC = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\cl.exe"
+$Enc:CXX = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7\Tools\cl.exe"
 cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel

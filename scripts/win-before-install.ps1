20fcbde323865222acd090a5c24de7a94aa802f9
Vito Gamberini <vito@gamberini.email>
2020-08-19 05:23:57 -0400

    try with conan

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -1,8 +1,10 @@
-choco install python pip swig cmake ninja visualstudio2019buildtools
+choco install python pip swig cmake ninja visualstudio2019buildtools conan
 refreshenv
+conan user
+conan install ./scripts
 
-git clone https://github.com/microsoft/vcpkg.git
-cd vcpkg
-./bootstrap-vcpkg.bat
-./vcpkg install --triplet x64-windows zlib botan boost-asio boost-log boost-interprocess
-./vcpkg integrate install
+# git clone https://github.com/microsoft/vcpkg.git
+# cd vcpkg
+# ./bootstrap-vcpkg.bat
+# ./vcpkg install --triplet x64-windows zlib botan boost-asio boost-log boost-interprocess
+# ./vcpkg integrate install

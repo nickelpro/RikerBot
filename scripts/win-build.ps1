d669d0c2cd3cb6e8e196fb4ece2717b9635211c0
Vito Gamberini <vito@gamberini.email>
2020-08-20 04:19:30 -0400

    Do it the windows way

--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,8 +1,7 @@
-$Env:VS160COMNTOOLS = "C:\BuildTools\Common7\Tools"
 $Env:Python_ROOT_DIR = "C:\Python38"
-Get-ChildItem -Path "C:\BuildTools" -Recurse
+C:\BuildTools\Common7\Tools\VsDevCmd.bat -help
 C:\BuildTools\Common7\Tools\VsDevCmd.bat
-cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
+cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G "Visual Studio 16 2019" -A x64
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel
 type CMakeFiles/CMakeOutput.log

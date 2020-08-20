0bda243c57c2a935fbb00aae0438d49202af47a8
Vito Gamberini <vito@gamberini.email>
2020-08-20 03:46:53 -0400

    Once more into the breach

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -3,8 +3,14 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 choco install python swig cmake ninja
-choco install visualstudio2019buildtools --package-parameters "--add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 refreshenv
 pip install wheel
 pip install conan
 conan install ./scripts --profile ./scripts/msvc_profile
+
+Import-Module BitsTransfer
+Start-BitsTransfer -Source "https://aka.ms/vs/16/release/vs_buildtools.exe" -Destination "C:\vs_buildtools.exe"
+
+New-Item -ItemType directory -Path C:\BuildTools
+
+C:\vs_buildtools.exe --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,7 +1,8 @@
-$Env:VS160COMNTOOLS = "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\Common7"
+$Env:VS160COMNTOOLS = "C:\BuildTools\Common7\Tools"
 $Env:CC = "cl.exe"
 $Env:CXX = "cl.exe"
 $Env:Python_ROOT_DIR = "C:\Python38"
+C:\BuildTools\Common7\Tools\VsDevCmd.bat
 cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -DCMAKE_CXX_COMPILER="cl.exe" -DCMAKE_CC_COMPILER="cl.exe" -G Ninja
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel

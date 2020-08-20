407ad917eb780ef472bf775c39e6589f9226b64d
Vito Gamberini <vito@gamberini.email>
2020-08-20 04:33:24 -0400

    Back to sln

--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,7 +1,6 @@
 $Env:Python_ROOT_DIR = "C:\Python38"
 C:\BuildTools\Common7\Tools\VsDevCmd.bat -arch=amd64 -host_arch=amd64
-#cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G "Visual Studio 16 2019" -A x64
-cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
+cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G "Visual Studio 16 2019" -A x64
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel
 type CMakeFiles/CMakeOutput.log

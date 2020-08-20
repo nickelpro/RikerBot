77ad6afad70dd8ccfdb7d3783818e6d38c6bcf6c
Vito Gamberini <vito@gamberini.email>
2020-08-20 04:00:00 -0400

    please

--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,9 +1,7 @@
 $Env:VS160COMNTOOLS = "C:\BuildTools\Common7\Tools"
-$Env:CC = "cl.exe"
-$Env:CXX = "cl.exe"
 $Env:Python_ROOT_DIR = "C:\Python38"
-C:\BuildTools\Common7\Tools\VsDevCmd.bat
-cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -DCMAKE_CXX_COMPILER="cl.exe" -DCMAKE_CC_COMPILER="cl.exe" -G Ninja
+Get-ChildItem -Path "C:\BuildTools" -Recurse
+C:\BuildTools\Common7\Tools\VsDevCmd.bat && cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel
 type CMakeFiles/CMakeOutput.log

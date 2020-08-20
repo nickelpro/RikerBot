eb1ffb39386f92381463571311a4274f0440203d
Vito Gamberini <vito@gamberini.email>
2020-08-20 04:05:51 -0400

    Sorry powershell I didn't mean it

--- scripts/win-build.ps1
+++ scripts/win-build.ps1
@@ -1,7 +1,8 @@
 $Env:VS160COMNTOOLS = "C:\BuildTools\Common7\Tools"
 $Env:Python_ROOT_DIR = "C:\Python38"
 Get-ChildItem -Path "C:\BuildTools" -Recurse
-C:\BuildTools\Common7\Tools\VsDevCmd.bat && cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
+C:\BuildTools\Common7\Tools\VsDevCmd.bat
+cmake . -DCMAKE_TOOLCHAIN_FILE=./conan_paths.cmake -DPython_ROOT_DIR=C:\Python38 -G Ninja
 cmake --build . --target rikerbot_all
 python setup.py bdist_wheel
 type CMakeFiles/CMakeOutput.log

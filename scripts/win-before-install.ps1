6779aee80c58baada036c5ddfbbc60b9f5119621
Vito Gamberini <vito@gamberini.email>
2020-08-20 03:52:40 -0400

    Back to choco

--- scripts/win-before-install.ps1
+++ scripts/win-before-install.ps1
@@ -3,14 +3,9 @@ if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
 }
 choco install python swig cmake ninja
+New-Item -ItemType directory -Path C:\BuildTools
+choco install visualstudio2019buildtools --package-parameters "--norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
 refreshenv
 pip install wheel
 pip install conan
 conan install ./scripts --profile ./scripts/msvc_profile
-
-Import-Module BitsTransfer
-Start-BitsTransfer -Source "https://aka.ms/vs/16/release/vs_buildtools.exe" -Destination "C:\vs_buildtools.exe"
-
-New-Item -ItemType directory -Path C:\BuildTools
-
-C:\vs_buildtools.exe --norestart --nocache --installPath C:\BuildTools --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64
